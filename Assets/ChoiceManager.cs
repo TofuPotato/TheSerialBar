4c312f8e36c011ea2b4431f2bb414705dff7e0dd
TofuPotato <ngyungqi@gmail.com>
2024-06-26 19:02:39 +0800

    update buttons to start game logic, clipboard buttons, and yes and no buttons

---
+++ Assets/ChoiceManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceManager : MonoBehaviour
+{
+
+    public void checkChoice()
+    {
+        Debug.Log("test");
+    }
+}
---
+++ Assets/ClickManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    public GameObject id;
+    public Sprite idSprite;
+
+    public void CheckChoice(Object button)
+    {
+        if(button.name ==  "YesButton")
+        {
+            Debug.Log("YES");
+        }
+        else
+        {
+            Debug.Log("NO");
+        }
+
+        id = GameObject.FindWithTag("ID");
+        Debug.Log(id.name);
+        Destroy(id);
+
+        id = new GameObject();
+        id.name = "BordieCollinsID";
+
+    }
+}
