544066cb0afe2eae7b7a3df315e3db5b0386d38e
TofuPotato <ngyungqi@gmail.com>
2024-07-25 19:40:28 +0800

    add spawn array of ids and check choice

--- Assets/ClickManager.cs
+++ Assets/ClickManager.cs
@@ -5,25 +5,52 @@ using UnityEngine;
 public class ClickManager : MonoBehaviour
 {
     public GameObject id;
-    public Sprite idSprite;
+    public GameObject[] idPrefabs;
+    public GameObject[] newIDs;
+    public int i;
 
     public void CheckChoice(Object button)
     {
+        // Add logic to check whether player choice is correct
+        id = GameObject.FindWithTag("ID");
+        Debug.Log(id.name);
+        // Check if name contains "wrong"
+        if(id.name.Contains("Wrong"))
+        {
+            Debug.Log(id.name);
+            Debug.Log("Wrong");
+        }
+        else
+        {
+            Debug.Log(id.name);
+            Debug.Log("Correct");
+        }
+
+        
         if(button.name ==  "YesButton")
         {
             Debug.Log("YES");
+            // Update score or story point
         }
         else
         {
             Debug.Log("NO");
+            // Update score or story point
         }
 
-        id = GameObject.FindWithTag("ID");
-        Debug.Log(id.name);
-        //Destroy(id);
+        // Remove current ID 
+        Destroy(id);
 
-        id = new GameObject();
-        id.name = "BordieCollinsID";
-
+        // Spawn next in line ID
+        if(i < idPrefabs.Length)
+        {
+            newIDs = new GameObject[idPrefabs.Length];
+            newIDs[i] = Instantiate(idPrefabs[i]) as GameObject;
+            i++;
+        }
+        else
+        {
+            Debug.Log("Game End");
+        }
     }
 }
