0462aa80dc7f1b9a81939ad8eaccde1fecfda62b
TofuPotato <ngyungqi@gmail.com>
2024-08-06 22:06:22 +0800

    update animations and UI

--- Assets/ClickManager.cs
+++ Assets/ClickManager.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ClickManager : MonoBehaviour
 {
@@ -8,49 +9,90 @@ public class ClickManager : MonoBehaviour
     public GameObject[] idPrefabs;
     public GameObject[] newIDs;
     public int i;
+    public int scoreCount;
+    public TextMeshProUGUI scoreText;
+    // public GameObject queue;
+    // public GameObject[] npcs;
+    // public GameObject currentNPC;
+    // public Transform target;
+    // public Animator[] animators;
 
     public void CheckChoice(Object button)
     {
-        // Add logic to check whether player choice is correct
+        // Get current ID displayed
         id = GameObject.FindWithTag("ID");
-        Debug.Log(id.name);
-        // Check if name contains "wrong"
-        if(id.name.Contains("Wrong"))
-        {
-            Debug.Log(id.name);
-            Debug.Log("Wrong");
-        }
-        else
-        {
-            Debug.Log(id.name);
-            Debug.Log("Correct");
-        }
-
         
-        if(button.name ==  "YesButton")
+        if(id != null)
         {
-            Debug.Log("YES");
-            // Update score or story point
-        }
-        else
-        {
-            Debug.Log("NO");
-            // Update score or story point
-        }
+            // npcs = new GameObject[queue.transform.childCount];
+            // for(int n = 0; n < npcs.Length; n++)
+            // {
+            //     npcs[n] = queue.transform.GetChild(n).gameObject;
+            //     if(id.name.Contains(npcs[n].name))
+            //     {
+            //         currentNPC = npcs[n];
+            //         Debug.Log(currentNPC.name);
+            //     }
+            // }
 
-        // Remove current ID 
-        Destroy(id);
+            // Check whether player choice is correct
+            if(button.name ==  "YesButton") // Allow entry
+            {
+                if(id.name.Contains("Wrong"))
+                {
+                    // Should not allow entry, deduct point
+                    Debug.Log("Wrong");
+                    scoreCount = int.Parse(scoreText.text);
+                    scoreCount -= 1;
+                    scoreText.text = scoreCount.ToString();
+                }
+                else
+                {
+                    // Allow entry, add point
+                    Debug.Log("Correct");
+                    scoreCount = int.Parse(scoreText.text);
+                    scoreCount += 1;
+                    scoreText.text = scoreCount.ToString();
+                }
+                // for(int a = 0; a < animators.Length; a++)
+                // {
+                //     animators[a].SetBool("IsWalk", true);
+                // }
+            }
+            else // Deny entry
+            {
+                if(id.name.Contains("Wrong"))
+                {
+                    // Deny entry, add point
+                    Debug.Log("Correct");
+                    scoreCount = int.Parse(scoreText.text);
+                    scoreCount += 1;
+                    scoreText.text = scoreCount.ToString();
+                }
+                else
+                {
+                    // Should allow entry, deduct point
+                    Debug.Log("Wrong");
+                    scoreCount = int.Parse(scoreText.text);
+                    scoreCount -= 1;
+                    scoreText.text = scoreCount.ToString();
+                }
+            }
 
-        // Spawn next in line ID
-        if(i < idPrefabs.Length)
-        {
-            newIDs = new GameObject[idPrefabs.Length];
-            newIDs[i] = Instantiate(idPrefabs[i]) as GameObject;
-            i++;
-        }
-        else
-        {
-            Debug.Log("Game End");
+            // Remove current ID 
+            Destroy(id);
+
+            // Spawn next ID in queue
+            if(i < idPrefabs.Length)
+            {
+                newIDs = new GameObject[idPrefabs.Length];
+                newIDs[i] = Instantiate(idPrefabs[i]) as GameObject;
+                i++;
+            }
+            else
+            {
+                Debug.Log("Game End");
+            }
         }
     }
 }
